224d37586172ede1959596660528f3d58ffc6d1e
Jacob M <jmayereup@gmail.com>
2023-10-08 14:34:45 +0700

    Setting Up With Docker (#1)
    * sync
    
    * sync

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        teacherjake:
+        {
+          "primary": "#006400",
+          "bg-primary": "#006400",
+          "secondary": "wheat",
+          "accent": "#a78bfa",
+          "neutral": "#2b3440",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+
+        }
+      }
+    ],
+  },
+}
+
